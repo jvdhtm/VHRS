b612d7cd60ebb24bb3fa1c36fcf37edf28760755
jvdhtm <jvdhtm@gmail.com>
2024-06-25 20:49:31 +0200

    add: add back the models

--- VectorHRS-frontend/packages/cli/src/index.ts
+++ VectorHRS-frontend/packages/cli/src/index.ts
@@ -1,2 +1,6 @@
 import  generateResources  from "./modules/generateResources";
+import  generateSchemas  from "./modules/generateSchemas"
+
+
 generateResources("../../packages/resources/src/servers/resources/");
+generateSchemas("../../packages/resources/src/servers/types/");
